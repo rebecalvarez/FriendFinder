f1fd39f4c8e9492107bd2c5b120bd582cf384349
rebecalvarez <rebeca.dodero@gmail.com>
2019-01-15 10:24:34 -0600

    routings

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,14 +1,26 @@
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./../public/home.html"));
+
+var path = require("path");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+
+
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
   
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "./../public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
   
-
-  // Router
-
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
-  
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+}
